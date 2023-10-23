b7104f35c56a2099c720c6cb3e2661049fea15ec
vasanthabalanm <vasanthabalanm.kanini@gmail.com>
2023-10-24 00:40:07 +0530

    test data checed

---
+++ TestWeatherData/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestWeatherData
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ TestWeatherDatas/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestWeatherDatas
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
